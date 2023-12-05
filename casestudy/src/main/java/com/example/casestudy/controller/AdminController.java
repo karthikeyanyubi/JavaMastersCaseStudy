48cf88f9efe61d401e2521df57b17653f2aa4eb3
karthikeyanyubi <karthikeyan.seethapathy@go-yubi.com>
2023-12-05 11:05:18 +0530

    "Adding Global Exceptions"

--- casestudy/src/main/java/com/example/casestudy/controller/AdminController.java
+++ casestudy/src/main/java/com/example/casestudy/controller/AdminController.java
@@ -9,46 +9,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
 
 @RestController
-public class UserController {
+@RequestMapping("/admin")
+public class AdminController {
 
     @Autowired
     private UserService userService;
 
-    @PostMapping("/allUsers/signUp")
-    public ResponseEntity<UserResponseDto> signUpUser(@RequestBody UserAccount user) {
-        try
-        {
-            UserResponseDto signedUpUser= userService.signUp(user);
-            return ResponseEntity.status(HttpStatus.OK).body(signedUpUser);
-
-        }
-        catch (Exception e)
-        {
-            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(UserResponseDto.builder().message(e.getMessage()).build());
-        }
-    }
-
-    @PostMapping("/allUsers/login")
-    public ResponseEntity<UserResponseDto> login (@RequestBody UserAccount user) {
-        try
-        {
-            UserResponseDto loggedInUser= userService.login(user);
-            return ResponseEntity.status(HttpStatus.OK).body(loggedInUser);
-
-        }
-        catch (Exception e)
-        {
-            return  ResponseEntity.status(HttpStatus.BAD_REQUEST).body(UserResponseDto.builder().message(e.getMessage()).build());
-        }
-
-
-    }
-
-    @PostMapping("/admin/updateRole")
+    @PostMapping("/updateRole")
     public ResponseEntity<UserResponseDto> updateRole(@RequestBody UserAccount user) {
         try
         {
@@ -62,7 +32,7 @@ public class UserController {
         }
     }
 
-    @GetMapping("/admin/getUsers")
+    @GetMapping("/getUsers")
     public ResponseEntity<Object> getUsers() {
         try
         {
--- casestudy/src/main/java/com/example/casestudy/model/UserAccount.java
+++ casestudy/src/main/java/com/example/casestudy/model/UserAccount.java
@@ -9,8 +9,8 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
 import java.util.Date;
-import java.util.UUID;
 
 
 @Entity
@@ -24,16 +24,18 @@ public class UserAccount {
     private Long userId;
 
     @Column(unique = true, name = "user_login_id")
+    @NotBlank(message = "Login ID cannot be blank")
     private String loginId;
 
     @Column(name = "user_pass")
+    @NotBlank(message = "Password cannot be blank")
     private String password;
 
     @Column(name = "token")
     private String token;
 
     @Column(columnDefinition = "BOOLEAN DEFAULT 'false'", name = "is_user_admin")
-    private Boolean isAdmin;
+    private Boolean isAdmin = false;
 
     @CreationTimestamp
     @Column(name = "created_date", nullable = false, updatable = false)
